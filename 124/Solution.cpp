f37b7413bba2e9caa64a4ab80f9b3bcd3826e05c
Bob <zdh990831@163.com>
2020-12-28 21:31:17 +0800

    1

---
+++ 124/Solution.cpp
@@ -0,0 +1,54 @@
+
+/** 
+ * 给定一个非空二叉树，返回其最大路径和。
+ * https://leetcode-cn.com/problems/binary-tree-maximum-path-sum/
+ **/
+
+
+/**
+ * Definition for a binary tree node. 
+ */
+
+
+#include <climits>
+#include <algorithm>
+
+
+using namespace std;
+
+struct TreeNode {
+    int val;
+    TreeNode *left;
+    TreeNode *right;
+    TreeNode() : val(0), left(nullptr), right(nullptr) {}
+    TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+    TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+};
+
+
+class Solution {
+
+private:
+	int ans = INT_MIN;
+
+public:
+
+    int maxPathSum(TreeNode* root) 
+    {
+    	findMax(root);
+    	return ans;
+    }
+
+    int findMax (TreeNode* node)
+    {
+    	int sum = node->val;
+    	int left_max  = 0;
+    	int right_max = 0;
+    	if (node->left != nullptr)  left_max  = max(left_max,  findMax(node->left) );
+    	if (node->right != nullptr) right_max = max(right_max, findMax(node->right));
+
+
+        ans = max(ans, sum+left_max+right_max);
+    	return sum + max(left_max, right_max);
+    }
+};
