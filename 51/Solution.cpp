d78d32aa008351602fff42aafc225220531d446e
Denghui <zdh990831@163.com>
2020-12-31 17:14:56 +0800

    1

---
+++ 51/Solution.cpp
@@ -0,0 +1,16 @@
+/*
+* @Author: Denghui
+* @Date:   2020-12-31 17:14:30
+* @Last Modified by:   Denghui
+* @Last Modified time: 2020-12-31 17:14:38
+*/
+
+
+
+class Solution {
+public:
+    vector<vector<string>> solveNQueens(int n) {
+        vecotor < <vector> <string> > ans;
+        
+    }
+};
